b32ee0291bcc17d1f6de2d1a1f9a166e61cdf54d
Vaishvi <vaishvi.sce20@sot.pdpu.ac.in>
2024-10-28 00:52:50 -0400

    Made some part of responsive landing page

--- frontend/src/Pages/Auth/landingPage.tsx
+++ frontend/src/Pages/Auth/landingPage.tsx
@@ -4,20 +4,20 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ overflow: "hidden" }}>
+    <div style={{ overflow: "hidden", padding: "20px" }}>
       <div
         style={{
-          position: "absolute",
-          width: "523px",
-          height: "96px",
-          left: "62px",
-          top: "269px",
+          width: "90%",
+          maxWidth: "523px",
+          margin: "auto",
           fontFamily: "Urbanist",
           fontStyle: "normal",
           fontWeight: 600,
-          fontSize: "40px",
-          lineHeight: "48px",
+          fontSize: "6vw",
+          lineHeight: "1.2",
+          textAlign: "center",
           color: "#000000",
+          marginTop: "10vh",
         }}
       >
         We understand that being a student can be{" "}
@@ -26,17 +26,16 @@ const LandingPage = () => {
 
       <div
         style={{
-          position: "absolute",
-          width: "550px",
-          height: "72px",
-          left: "62px",
-          top: "383px",
+          width: "90%",
+          maxWidth: "550px",
+          margin: "20px auto",
           fontFamily: "Urbanist",
           fontStyle: "normal",
           fontWeight: "400",
-          fontSize: "20px",
-          lineHeight: "24px",
+          fontSize: "4vw",
+          lineHeight: "1.2",
           color: "#666666",
+          textAlign: "center",
         }}
       >
         Join our dynamic team right here on campus. Earn, learn, and be part of
@@ -44,115 +43,117 @@ const LandingPage = () => {
         your campus experience!
       </div>
 
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          navigate("/register");
-        }}
-        type="button"
+      <div
         style={{
-          position: "absolute",
-          width: "223px",
-          height: "54px",
-          left: "62px",
-          top: "501px",
-          background: "#FF5353",
-          borderRadius: "10px",
           display: "flex",
-          alignItems: "center",
           justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+          marginTop: "10px",
         }}
       >
-        <p
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/register");
+          }}
+          type="button"
           style={{
-            fontFamily: "Urbanist",
-            fontStyle: "normal",
-            fontWeight: 600,
-            fontSize: "20px",
-            lineHeight: "24px",
-            color: "white",
-            margin: 0,
+            width: "40%",
+            maxWidth: "223px",
+            height: "54px",
+            background: "#FF5353",
+            borderRadius: "10px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          Sign Up
-        </p>
-      </button>
+          <p
+            style={{
+              fontFamily: "Urbanist",
+              fontStyle: "normal",
+              fontWeight: 600,
+              fontSize: "4vw",
+              lineHeight: "1.2",
+              color: "white",
+              margin: 0,
+            }}
+          >
+            Sign Up
+          </p>
+        </button>
 
-      <p
-        style={{
-          position: "absolute",
-          width: "29px",
-          height: "30px",
-          left: "308px",
-          top: "513px",
-          fontFamily: "Poppins",
-          fontStyle: "normal",
-          fontWeight: 500,
-          fontSize: "20px",
-          lineHeight: "30px",
-          textAlign: "center",
-          color: "#8C8D90",
-        }}
-      >
-        OR
-      </p>
-
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          navigate("/login");
-        }}
-        type="button"
-        style={{
-          boxSizing: "border-box",
-          position: "absolute",
-          width: "223px",
-          height: "54px",
-          left: "359px",
-          top: "501px",
-          background: "#FFFFFF",
-          border: "1px solid #656565",
-          borderRadius: "10px",
-        }}
-      >
         <p
           style={{
-            fontFamily: "Urbanist",
+            fontFamily: "Poppins",
             fontStyle: "normal",
-            fontWeight: 600,
-            fontSize: "20px",
-            lineHeight: "24px",
-            margin: 0,
-            color: "#656565",
+            fontWeight: 500,
+            fontSize: "4vw",
+            color: "#8C8D90",
+            margin: "0 10px",
           }}
         >
-          Login
+          OR
         </p>
-      </button>
 
-      <div
-        style={{
-          position: "absolute",
-          width: "735px",
-          height: "752px",
-          left: "669px",
-          top: "121px",
-          mixBlendMode: "multiply",
-        }}
-      >
-        <img src="/images/landingpage_image1.png" alt="Landing Page Image" />
-      </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/login");
+          }}
+          type="button"
+          style={{
+            boxSizing: "border-box",
+            position: "absolute",
+            width: "223px",
+            height: "54px",
+            left: "359px",
+            top: "501px",
+            background: "#FFFFFF",
+            border: "1px solid #656565",
+            borderRadius: "10px",
+          }}
+        >
+          <p
+            style={{
+              fontFamily: "Urbanist",
+              fontStyle: "normal",
+              fontWeight: 600,
+              fontSize: "20px",
+              lineHeight: "24px",
+              margin: 0,
+              color: "#656565",
+            }}
+          >
+            Login
+          </p>
+        </button>
 
-      <div
-        style={{
-          position: "absolute",
-          width: "420px",
-          height: "640px",
-          left: "612px",
-          top: "167px",
-        }}
-      >
-        <img src="/images/landingpage_image2.png" alt="Landing Page Image" />
+        <div
+          style={{
+            position: "absolute",
+            width: "735px",
+            height: "752px",
+            left: "669px",
+            top: "121px",
+            mixBlendMode: "multiply",
+          }}
+        >
+          <img src="/images/landingpage_image1.png" alt="Landing Page Image" />
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            width: "420px",
+            height: "640px",
+            left: "612px",
+            top: "167px",
+          }}
+        >
+          <img src="/images/landingpage_image2.png" alt="Landing Page Image" />
+        </div>
       </div>
     </div>
   );
